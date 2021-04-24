d0ee640433011511246a4ef97cce4798578bf774
super-hxman <shahanahbp@gmail.com>
2021-04-24 17:12:14 +0400

    call API endpoint

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -9,6 +9,19 @@ export default class Room extends Component{
             isHost: false,
         };
         this.roomCode = this.props.match.params.roomCode;
+        this.getRoomDetails();
+    }
+
+    getRoomDetails() {
+        fetch("/api/get-room" + "?code=" + this.roomCode)
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                votesToSkip: data.votes_to_skip,
+                guestCanPause: data.guest_can_pause,
+                isHost: data.is_host,
+                });
+        });
     }
 
     render(){
@@ -18,10 +31,10 @@ export default class Room extends Component{
                 Votes: {this.state.votesToSkip}
             </p>
             <p>
-                Guests Can Pause: {this.state.guestCanPause}
+                Guests Can Pause: {this.state.guestCanPause.toString()}
             </p>
             <p>
-                Host: {this.state.isHost}
+                Host: {this.state.isHost.toString()}
             </p>
         </div>
     }
